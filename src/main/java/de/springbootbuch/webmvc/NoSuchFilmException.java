d603f50ae5f21a6859d40f1874dc7ca3212b07cf
Michael J. Simons <michael@simons.ac>
2017-05-01 19:48:19 +0200

    Work on the source layout

--- src/main/java/de/springbootbuch/webmvc/NoSuchFilmException.java
+++ src/main/java/de/springbootbuch/webmvc/NoSuchFilmException.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @author @rotnroll666
  */
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class NoSuchFilmException extends RuntimeException {
+public class NoSuchFilmException 
+	extends RuntimeException {
 	private static final long serialVersionUID = -1469903817218715717L;
 }
